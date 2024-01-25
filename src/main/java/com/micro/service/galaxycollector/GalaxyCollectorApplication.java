1e0fd7d9ee6264499569ddc745e821b2385a3acf
barney <wang.rui.barney@vertiv.com>
2024-01-25 16:37:12 +0800

    init

---
+++ src/main/java/com/micro/service/galaxycollector/GalaxyCollectorApplication.java
@@ -0,0 +1,15 @@
+package com.micro.service.galaxycollector;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.micro.service.galaxycore.dao")
+public class GalaxyCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyCollectorApplication.class, args);
+	}
+
+}
